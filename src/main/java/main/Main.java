874f3b509fc258dffc1809f6fa8d2b86a3852056
Joel Kamps <joel.kamps@peakwork.com>
2017-10-27 10:14:37 +0200

    first two

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,8 +1,12 @@
 package main;
 
+import tasks.ProjectEulerSolutions;
+
 public class Main {
 
     public static void main (String[] args) {
-
+        ProjectEulerSolutions projectEulerSolutions= new ProjectEulerSolutions();
+        projectEulerSolutions.multipleOf3And5();
+        projectEulerSolutions.evenFibonacciNumbers();
     }
 }
